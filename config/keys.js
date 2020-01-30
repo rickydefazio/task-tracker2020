90e260813523e90feab484e57f90980f45877986
Ricky DeFazio <36946678+RickyDeFazio@users.noreply.github.com>
2020-01-30 01:31:33 -0500

    Change db name for uri

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
   mongoURI:
-    'mongodb+srv://rdefazio:VmMCw1y3xatyUB5o@cluster0-vpxrn.mongodb.net/task_tracker?retryWrites=true&w=majority'
+    'mongodb+srv://rdefazio:VmMCw1y3xatyUB5o@cluster0-vpxrn.mongodb.net/test?retryWrites=true&w=majority'
 };
